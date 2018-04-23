b4847893e3f1dbda6bfafd05dbf04a2fed9aabbf
marcod1419 <marcod1419@gmail.com>
2018-04-23 11:15:20 -0400

    docs(docs): remove heading import

--- docs/components/custom/TdsCore/TdsCore.jsx
+++ docs/components/custom/TdsCore/TdsCore.jsx
@@ -2,4 +2,4 @@ import Box from '@tds/core-box'
 import HairlineDivider from '@tds/core-hairline-divider'
 import ChevronLink from '@tds/core-chevron-link'
 
-global = Object.assign(global,{Box, HairlineDivider, ChevronLink, Heading})
+global = Object.assign(global,{Box, HairlineDivider, ChevronLink})

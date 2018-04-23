81723d8af4f5de74329b614a40511191f48cc965
marcod1419 <marcod1419@gmail.com>
2018-04-23 16:06:48 -0400

    chore(docs): add additional core components to docs

--- docs/setup/tds-core-globals.js
+++ docs/setup/tds-core-globals.js
@@ -1,5 +1,31 @@
-import Box from '@tds/core-box'
-import HairlineDivider from '@tds/core-hairline-divider'
-import ChevronLink from '@tds/core-chevron-link'
+import Box from "@tds/core-box";
+import ChevronLink from "@tds/core-chevron-link";
+import CoreColours from "@tds/core-colours";
+import CssReset from "@tds/core-css-reset";
+import FlexGrid from "@tds/core-flex-grid";
+import HairlineDivider from "@tds/core-hairline-divider";
+import Heading from "@tds/core-heading";
+import Input from "@tds/core-input";
+import Link from "@tds/core-link";
+import OrderedList from "@tds/core-ordered-list";
+import Paragraph from "@tds/core-paragraph";
+import Strong from "@tds/core-strong";
+import Text from "@tds/core-text";
+import UnorderedList from "@tds/core-unordered-list";
 
-Object.assign(global, {Box, HairlineDivider, ChevronLink})
+Object.assign(global, {
+  Box,
+  ChevronLink,
+  CoreColours,
+  CssReset,
+  FlexGrid,
+  HairlineDivider,
+  Heading,
+  Input,
+  Link,
+  OrderedList,
+  Paragraph,
+  Strong,
+  Text,
+  UnorderedList
+});

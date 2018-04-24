54b8ba8b3d72b3e611c5ecc78f7f3fed4e4c8305
marcod1419 <marcod1419@gmail.com>
2018-04-24 10:58:54 -0400

    test(docs): repair markdown heading path in test

--- docs/components/custom/MarkdownHeading/__tests__/MarkdownHeading.spec.jsx
+++ docs/components/custom/MarkdownHeading/__tests__/MarkdownHeading.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import MarkdownHeading from '../MarkdownHeading'
-import Heading from '../../../../../packages/Heading/Heading'
+import Heading from '@tds/core-heading'
 
 describe('MarkdownHeading', () => {
   it('renders', () => {

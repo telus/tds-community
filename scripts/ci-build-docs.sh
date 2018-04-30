8512fb776ff4f47eceffd7e818c824f7a71401c5
marcod1419 <marcod1419@gmail.com>
2018-04-30 15:18:32 -0400

    chore(build): add openshift pipeline scripts

---
+++ scripts/ci-build-docs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Strategy:
+# Step 1. Build gitbook and styleguidist in parallel for speed
+# Step 2. Copy styleguidist into build/${ENV}/components
+
+# 1
+yarn ci:build-docs
+
+mkdir -p build/staging
+mkdir -p build/production
+
+# 2
+cp -R styleguide/staging/ build/staging/components
+cp -R styleguide/production/ build/production/components

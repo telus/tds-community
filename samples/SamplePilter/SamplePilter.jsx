bd7e9898171b6cd74c2bfe3e6dee5aca24635ade
Enrico Sacchetti <esacchetti@gmail.com>
2018-06-26 10:13:23 -0400

    chore(community-sample-pilter): fix safeRest import (#75)

--- samples/SamplePilter/SamplePilter.jsx
+++ samples/SamplePilter/SamplePilter.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Box from '@tds/core-box'
 import Text from '@tds/core-text'
 
-import safeRest from '@tds/shared-safe-rest'
+import safeRest from '../../shared/safe-rest/safeRest'
 
 import styles from './SamplePilter.scss'
 

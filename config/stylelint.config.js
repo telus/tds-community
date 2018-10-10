7b0e975556a84b84417da594f81b38dc61d20e89
Enrico Sacchetti <enrico.sacchetti@telus.com>
2018-10-10 16:21:37 -0400

    chore(config): ignore node_modules in stylelint

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Don't use stylelint-config-css-modules as it overrides needed settings from recommended-scss
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
+  ignoreFiles: '**/node_modules/**',
   rules: {
     'property-no-unknown': [
       true,

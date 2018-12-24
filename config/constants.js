4a1ad17a80ad0ba348122992227476b65b979b0b
Marco Donnici <marcod1419@gmail.com>
2018-12-24 15:41:07 -0500

    chore(deps): update to lerna v3
    - add ignore package option
    - update commitlint config
    - update prePr script
    - update TDSBot to support new prePr script

---
+++ config/constants.js
@@ -0,0 +1,3 @@
+// Lists of TDS components to ignore during builds, tests, and publishes. Defined as an array of strings of packages. (""@tds/community-package-name")
+
+exports.ignoredPackages = []
---
+++ scripts/prePr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env expect
+
+#
+# Usage: yarn prepr
+#
+# This script will give insight into the result of publishing given the current commit history. It is intended to be run
+# before making a Pull Request to ensure its integrity.
+
+log_user 0
+spawn npx lerna version --conventional-commits --amend
+log_user 1
+
+set PROMPT [ expect "Are you sure you want to create these versions?"]
+if { $PROMPT != "" } {
+  send "n\r"
+}
+
+puts "\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again."

292c98c6e8ac2fd807e379bf9b203edf4d7d81d3
Marco Donnici <marcod1419@gmail.com>
2019-02-07 14:56:54 -0500

    chore(scripts): update for docker and npm, add git scope linting

--- scripts/utils/parseArgs.js
+++ scripts/utils/parseArgs.js
@@ -2,9 +2,16 @@ const parseArgs = require('minimist')
 
 const TDS_OPTIONS = ['-a', '--all', '-u', '--update-screenshots']
 
-const originalArgs = process.argv.slice(2)
+let originalArgs = process.argv.slice(2)
 
-const parsedArgs = parseArgs(originalArgs, { alias: { a: 'all', u: 'update-screenshots' } })
+if (originalArgs.length === 1) {
+  originalArgs = originalArgs[0].split(' ')
+}
+
+const parsedArgs = parseArgs(originalArgs, {
+  alias: { a: 'all', u: 'update-screenshots' },
+})
+
 const lernaOptions = originalArgs
   .filter(arg => !parsedArgs._.includes(arg))
   .filter(arg => !TDS_OPTIONS.includes(arg))

420751e9b45f95503d3b6e3fff6f55c3a7c55aaf
Marco Donnici <marcod1419@gmail.com>
2019-02-07 14:46:52 -0500

    chore(other): prepare root for docker

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 module.exports = function(api) {
   api.cache(true)
   return {
@@ -5,6 +6,10 @@ module.exports = function(api) {
     plugins: ['@babel/plugin-proposal-class-properties'],
     env: {
       test: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+      e2e: {
         presets: ['@babel/preset-env', '@babel/preset-react'],
         plugins: ['@babel/plugin-proposal-class-properties'],
       },

9dc047122614dcda3576509acb8dfb81dc75eb97
Marco Donnici <marcod1419@gmail.com>
2019-04-03 11:32:10 -0400

    chore(community-preview-card): add missing dependencies

--- packages/PreviewCard/index.cjs.js
+++ packages/PreviewCard/index.cjs.js
@@ -1,4 +1,3 @@
-require('./dist/index.css')
 const PreviewCard = require('./dist/index.cjs')
 
 module.exports = PreviewCard
--- packages/PreviewCard/index.es.js
+++ packages/PreviewCard/index.es.js
@@ -1,4 +1,3 @@
-import './dist/index.css'
 import PreviewCard from './dist/index.es'
 
 export default PreviewCard

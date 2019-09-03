c990a1268856d5dfa0f4b121731cfc6d09f76aff
Anatolii Gorovyi <anatolii.gorovyi@telus.com>
2019-09-03 15:15:20 -0400

    feat(community-skeleton-provider): upgrade to styled components
    BREAKING CHANGE: styled components now a peer dependency

--- packages/SkeletonProvider/__tests__/SkeletonProvider.spec.jsx
+++ packages/SkeletonProvider/__tests__/SkeletonProvider.spec.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Heading from '@tds/core-heading'
 import Text from '@tds/core-text'
 import Image from '@tds/core-image'
 import SkeletonProvider from '../SkeletonProvider'
 
 const doShallow = props => shallow(<SkeletonProvider {...props} />)
+const doMount = props => mount(<SkeletonProvider {...props} />)
 
 describe('SkeletonProvider', () => {
   it('should render children as skeletons', () => {
-    const skeleton = doShallow({
+    const skeleton = doMount({
       children: (
         <div>
           <Heading level="h1" skeleton>
             Hello
           </Heading>
-          <Image width={60} height={60} corners="rounded" src="" alt="skeleton image" />
+          <Image width={60} height={60} rounded="corners" src="" alt="skeleton image" skeleton />
           <Text skeleton={{ characters: 4, lines: 2 }}>Hello</Text>
         </div>
       ),
@@ -31,11 +32,94 @@ describe('SkeletonProvider', () => {
           <Heading level="h1" skeleton>
             Hello
           </Heading>
-          <Image width={60} height={60} corners="rounded" src="" alt="image" />
+          <Image width={60} height={60} rounded="corners" src="" alt="image" />
           <Text skeleton={{ characters: 4, lines: 2 }}>Hello</Text>
         </div>
       ),
     })
     expect(skeleton.render()).toMatchSnapshot()
+  })
+
+  it('allows additional attributes in skeleton', () => {
+    const skeleton = doShallow({
+      id: 'some-additiona-id',
+      'data-testid': 'some-id-added-for-e2e',
+      children: (
+        <div>
+          <Heading level="h1" skeleton>
+            Hello
+          </Heading>
+          <Image width={60} height={60} rounded="corners" src="" alt="skeleton image" />
+          <Text skeleton={{ characters: 4, lines: 2 }}>Hello</Text>
+        </div>
+      ),
+    })
+
+    expect(skeleton).toHaveProp('id', 'some-additiona-id')
+    expect(skeleton).toHaveProp('data-testid', 'some-id-added-for-e2e')
+  })
+
+  it('does not allow custom CSS', () => {
+    const skeleton = doMount({
+      children: (
+        <div>
+          <Heading
+            className="custom-heading-class"
+            style={{ background: 'red' }}
+            level="h1"
+            skeleton
+          >
+            Hello
+          </Heading>
+          <Image
+            className="custom-image-class"
+            style={{ background: 'red' }}
+            width={60}
+            height={60}
+            rounded="corners"
+            src=""
+            alt="skeleton image"
+            skeleton
+          />
+          <Text
+            className="custom-text-class"
+            style={{ background: 'red' }}
+            skeleton={{ characters: 4, lines: 2 }}
+          >
+            Hello
+          </Text>
+        </div>
+      ),
+    })
+
+    expect(
+      skeleton
+        .find('HeadingSkeleton')
+        .find('div')
+        .hasClass('custom-heading-class')
+    ).toEqual(false)
+    expect(skeleton.find('HeadingSkeleton').find('div')).not.toHaveProp('style')
+
+    expect(
+      skeleton
+        .find('ImageSkeleton')
+        .find('div')
+        .hasClass('custom-image-class')
+    ).toEqual(false)
+    expect(skeleton.find('ImageSkeleton').find('div')).not.toHaveProp('style')
+
+    expect(
+      skeleton
+        .find('TextSkeleton')
+        .find('div')
+        .first()
+        .hasClass('custom-text-class')
+    ).toEqual(false)
+    expect(
+      skeleton
+        .find('TextSkeleton')
+        .find('div')
+        .first()
+    ).not.toHaveProp('style')
   })
 })
--- packages/SkeletonProvider/tds/Image.jsx
+++ packages/SkeletonProvider/tds/Image.jsx
@@ -1,23 +1,58 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Image from '@tds/core-image'
+import styled from 'styled-components'
+import safeRest from '@tds/shared-safe-rest'
+import { colorGainsboro, colorAthensGrey } from '@tds/core-colours'
+
 import withSkeleton from '../withSkeleton'
-import styles from '../Skeleton.scss'
+
+const keyframes = require('styled-components').keyframes
+
+const skeletonTextShimmer = keyframes`
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -100% 0;
+  }
+`
+
+// Not using object notation as styled-components does not currently support animations in objects
+const getWidthHeight = ({ ...rest }) => `
+  width: ${rest.width * 0.8}px;
+  height: ${rest.height * 0.8}px;
+`
+
+const StyledImageSkeleton = styled.div`
+  ${getWidthHeight}
+  display: inline-block;
+  overflow: hidden;
+  max-width: 100%;
+  border-radius: 50%;
+
+  background-color: ${colorGainsboro};
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: 200% 100%;
+  background-image: linear-gradient(
+    to right,
+    ${colorGainsboro},
+    ${colorAthensGrey},
+    ${colorGainsboro}
+  );
+  animation-name: ${skeletonTextShimmer};
+  animation-duration: 2s;
+  animation-delay: 2s;
+  animation-iteration-count: infinite;
+`
 
 const ImageSkeleton = ({ skeleton, ...rest }) => {
   if (!skeleton) {
     return <Image {...rest} />
   }
 
-  const baseClass = styles['skeleton-image']
-  const circularClass = styles.circular
-
-  const cssWidth = `${rest.width * 0.8}px`
-  const cssHeight = `${rest.height * 0.8}px`
-
-  const classes = [baseClass, circularClass].join(' ')
-
-  return <div className={classes} style={{ width: cssWidth, height: cssHeight }} />
+  return <StyledImageSkeleton {...safeRest(rest)} />
 }
 
 ImageSkeleton.propTypes = {

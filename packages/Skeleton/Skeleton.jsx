64106038402afa3bb04b2ae447a4e00fe2f1e72d
Jordan Raffoul <me@jordanraffoul.com>
2019-09-12 15:22:18 -0400

    refactor(community-skeleton): use safeRest from @tds/util-helpers

--- packages/Skeleton/Skeleton.jsx
+++ packages/Skeleton/Skeleton.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { colorGainsboro, colorAthensGrey } from '@tds/core-colours'
-import safeRest from '@tds/shared-safe-rest'
+import { safeRest } from '@tds/util-helpers'
 
 const keyframes = require('styled-components').keyframes
 

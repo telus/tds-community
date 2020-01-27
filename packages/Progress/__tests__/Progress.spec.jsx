7eaf90786780a0b184d08740ec7ef85393e7ca3d
Marco Donnici <marcod1419@gmail.com>
2020-01-27 16:41:16 -0500

    feat(community-progress): implement new design standards
    feat: new size prop has been added with mini variant of progress bar
    feat: progress bars now have proper aria attributes
    BREAKING CHANGE: progress bar height has been adjusted and no longer changes with viewport
    BREAKING CHANGE: allyLabel prop in Progress is required to aid assistive technology
    BREAKING CHANGE: 'primary' variant is now named 'positive'
    BREAKING CHANGE: 'error' variant is now named 'negative'
    BREAKING CHANGE: removed 'secondary' variant
    BREAKING CHANGE: styled components is now a peer dependency

--- packages/Progress/__tests__/Progress.spec.jsx
+++ packages/Progress/__tests__/Progress.spec.jsx
@@ -4,27 +4,29 @@ import { shallow } from 'enzyme'
 import Progress from '../Progress'
 
 describe('Progress', () => {
-  const doShallow = (props = {}) =>
+  const defaultProps = {}
+  const doShallow = (overrides = {}) =>
     shallow(
-      <Progress {...props}>
-        <Progress.Bar percentage={0} />
+      <Progress {...defaultProps} {...overrides}>
+        <Progress.Bar percentage={10} a11yLabel="Data Usage" />
       </Progress>
     )
 
   it('renders', () => {
     const progress = doShallow()
-
     expect(progress).toMatchSnapshot()
   })
 
   it('does other things', () => {
     const progress = doShallow()
-
     expect(progress).toExist()
   })
 
   it('passes additional attributes to the element', () => {
-    const progress = doShallow({ id: 'the-id', 'data-some-attr': 'some value' })
+    const progress = doShallow({
+      id: 'the-id',
+      'data-some-attr': 'some value',
+    })
 
     expect(progress).toHaveProp('id', 'the-id')
     expect(progress).toHaveProp('data-some-attr', 'some value')

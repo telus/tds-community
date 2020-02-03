a451142a8150bf60553aaf7ca666d1a06ffdeeee
Marco Donnici <marcod1419@gmail.com>
2020-02-03 15:19:59 -0500

    chore(community-progress): add unique id generation to svgs

--- packages/Progress/Bar/textures/Negative.jsx
+++ packages/Progress/Bar/textures/Negative.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { uniqueId } from '@tds/util-helpers'
 
-const Negative = () => (
-  <svg width="100%" height="24">
-    <defs>
-      <pattern id="sasmd" patternUnits="userSpaceOnUse" width="8" height="8">
-        <rect width="8" height="8" fill="#C12335" />
-        <path
-          d="M 0,8 l 8,-8 M -2,2 l 4,-4 M 6,10 l 4,-4"
-          strokeWidth="2"
-          shapeRendering="auto"
-          stroke="#e7adb4"
-          strokeLinecap="square"
-        />
-      </pattern>
-    </defs>
-    <path d="M 0 0 L 0 140 L 2000 2000 L 2000 0 Z" style={{ fill: 'url("#sasmd")' }} />
-  </svg>
-)
+const Negative = () => {
+  const svgId = uniqueId('NegativeProgress')
+  return (
+    <svg width="100%" height="24">
+      <defs>
+        <pattern id={svgId} patternUnits="userSpaceOnUse" width="8" height="8">
+          <rect width="8" height="8" fill="#C12335" />
+          <path
+            d="M 0,8 l 8,-8 M -2,2 l 4,-4 M 6,10 l 4,-4"
+            strokeWidth="2"
+            shapeRendering="auto"
+            stroke="#e7adb4"
+            strokeLinecap="square"
+          />
+        </pattern>
+      </defs>
+      <path d="M 0 0 L 0 140 L 2000 2000 L 2000 0 Z" style={{ fill: `url("#${svgId}")` }} />
+    </svg>
+  )
+}
 
 export default Negative

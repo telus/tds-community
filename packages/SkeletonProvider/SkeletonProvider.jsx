b913e53b85a74f6483cbc7f668436473f2ba4b3a
Enrico Sacchetti <enrico.sacchetti@telus.com>
2020-04-01 11:43:48 -0400

    chore(community-skeleton-provider): adjust code formatting

--- packages/SkeletonProvider/SkeletonProvider.jsx
+++ packages/SkeletonProvider/SkeletonProvider.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 import { withContext } from 'recompose'
 import AutoSkeletonizer from './AutoSkeletonizer'
 
-const SkeletonProvider = withContext({ show: PropTypes.bool }, props => ({ show: props.show }))(
-  props => <Fragment>{AutoSkeletonizer(props.children)}</Fragment>
-)
+const SkeletonProvider = withContext({ show: PropTypes.bool }, props => ({
+  show: props.show,
+}))(props => <Fragment>{AutoSkeletonizer(props.children)}</Fragment>)
 
 SkeletonProvider.propTypes = {
   show: PropTypes.bool,

d27d4e0eb734388c997905e23627159e31fd1005
Enrico Sacchetti <enrico.sacchetti@telus.com>
2020-05-08 20:17:35 -0400

    chore(scripts): update scaffold files

--- scripts/scaffolding/Component.jsx
+++ scripts/scaffolding/Component.jsx
@@ -7,9 +7,23 @@ import { safeRest } from '@tds/util-helpers'
 /**
  * @version ./package.json
  */
-const $COMPONENT$ = ({ ...rest }) => <div {...safeRest(rest)} />
+const $COMPONENT$ = ({ prop1, prop2, ...rest }) => <div {...safeRest(rest)} />
 
-$COMPONENT$.propTypes = {}
+$COMPONENT$.propTypes = {
+  /**
+   * Use JSDoc to add documentation for every prop.
+   *
+   * Learn more at https://react-styleguidist.js.org/docs/documenting.html#using-jsdoc-tags
+   */
+  prop1: PropTypes.string.isRequired,
+  /**
+   * Use @ignore to hide a prop this component's prop table on Styleguidist
+   * And to denote a prop as 'unsupported'.
+   *
+   * Removal or props or changing default prop behaviour counts as a 'breaking change'.
+   */
+  prop2: PropTypes.func.isRequired,
+}
 
 $COMPONENT$.defaultProps = {}
 

f7f1a10e6dead0257d9526bef7d1aee0406b6393
Enrico Sacchetti <enrico.sacchetti@telus.com>
2020-05-08 22:45:59 -0400

    chore(scripts): adjust config paths for rollup
    update private folder name

--- scripts/scaffolding/rollup.config.js
+++ scripts/scaffolding/rollup.config.js
@@ -1,4 +1,4 @@
-import configure from '../../config/rollup.config'
+import configure from '$CONFIG_PATH$config/rollup.config'
 import { dependencies } from './package.json'
 
 export default configure({

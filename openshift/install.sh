9dcc5acef94c62f1ed1db1ce77bc1be5694263b2
Paul Saumets <psaumets@gmail.com>
2021-06-25 15:51:42 -0400

    fix(install): update deprecated flag for oc command

--- openshift/install.sh
+++ openshift/install.sh
@@ -10,15 +10,15 @@ cd `dirname $0`
 
 # Github Token
 vault read -field=ssh-key secret/common/github > id_rsa
-oc create secret generic github-secret --from-file=ssh-privatekey=id_rsa --dry-run -o yaml | oc apply -f -
+oc create secret generic github-secret --from-file=ssh-privatekey=id_rsa --dry-run=client -o yaml | oc apply -f -
 rm id_rsa
 
 # NPM Read and Publish Token
-oc create secret generic npmrc-secret --from-literal=.npmrc=$(vault read -field=npmrc secret/projects/npm-libraries/npm) --dry-run -o yaml | oc apply -f -
+oc create secret generic npmrc-secret --from-literal=.npmrc=$(vault read -field=npmrc secret/projects/npm-libraries/npm) --dry-run=client -o yaml | oc apply -f -
 
 # AWS
-oc create secret generic aws-client-secret --from-literal=aws.client=$(vault read -field=client secret/projects/o-design-outcomes/tds-s3) --dry-run -o yaml | oc apply -f -
-oc create secret generic aws-secret-key-secret --from-literal=aws.secret=$(vault read -field=secret secret/projects/o-design-outcomes/tds-s3) --dry-run -o yaml | oc apply -f -
+oc create secret generic aws-client-secret --from-literal=aws.client=$(vault read -field=client secret/projects/o-design-outcomes/tds-s3) --dry-run=client -o yaml | oc apply -f -
+oc create secret generic aws-secret-key-secret --from-literal=aws.secret=$(vault read -field=secret secret/projects/o-design-outcomes/tds-s3) --dry-run=client -o yaml | oc apply -f -
 
 # Install templates
 oc apply -f openshift-template.yml

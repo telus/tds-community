b196669f9c882f6b8e0860b218a0099d314dd296
Baljit Sokhi <baljit.sokhi@telus.com>
2021-06-28 15:18:53 -0400

    ribbon-fix

--- packages/Ribbon/Ribbon.jsx
+++ packages/Ribbon/Ribbon.jsx
@@ -7,27 +7,32 @@ import { safeRest } from '@tds/util-helpers'
 import { colorTelusPurple } from '@tds/core-colours'
 
 const StyledRibbon = styled.div({
-  backgroundColor: colorTelusPurple,
-  padding: '0px 1.5rem 0px 1rem',
-  position: 'relative',
-  display: 'inline-block',
+  // backgroundColor: colorTelusPurple,
+  // padding: '0px 1.5rem 0px 1rem',
+  // position: 'relative',
+  // display: 'inline-block',
   maxWidth: '100%',
   minWidth: '7.875rem',
   height: '1.5rem',
   fontWeight: 500,
-  '&:after': {
-    content: '""',
-    position: 'absolute',
-    right: '-0.3125rem',
-    bottom: '-0.5rem',
-    width: 0,
-    height: 0,
-    borderRight: '0.8125rem solid white',
-    borderTop: '1.25rem solid transparent',
-    borderBottom: '1.25rem solid transparent',
-  },
+  // '&:after': {
+  //   content: '""',
+  //   position: 'absolute',
+  //   right: '-0.3125rem',
+  //   bottom: '-0.5rem',
+  //   width: 0,
+  //   height: 0,
+  //   borderRight: '0.8125rem solid transparent',
+  //   borderTop: '1.25rem solid transparent',
+  //   borderBottom: '1.25rem solid transparent',
+  // },
   '> span': {
     lineHeight: '1 !important',
+
+    padding: '0.35rem 1.75rem 0.25rem 1rem',
+    display: 'inline-block',
+    height: '1.5rem',
+    background: `conic-gradient(from 90deg at 90% 50%,transparent 14%, ${colorTelusPurple} 16%, ${colorTelusPurple} 84%,transparent 86%)`,
   },
 })
 

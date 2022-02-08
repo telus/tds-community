18930c896d537cdc12fb162ba1387d88187a7eef
Layne Geck <layne.geck@telus.com>
2022-02-08 15:15:30 -0800

    fix: attempt to get gha working

---
+++ scripts/gha/github-pr.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+// Environment: These variables must be available in the environment.
+// GITHUB_TOKEN
+// GITHUB_REPOSITORY
+// GITHUB_PULL_REQUEST
+
+const request = require('request')
+const { spawnSync } = require('child_process')
+
+if (
+  !process.env.GITHUB_TOKEN ||
+  !process.env.GITHUB_REPOSITORY ||
+  !process.env.GITHUB_PULL_REQUEST
+) {
+  console.error(
+    "'GITHUB_TOKEN', 'GITHUB_REPOSITORY'., and 'GITHUB_PULL_REQUEST' must be available in the environment."
+  )
+  process.exit(1)
+}
+
+const { output } = spawnSync('scripts/prePr.sh')
+
+const changes = `Packages pending updates:\n${formattedVersions}\n\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again.`
+request.post({
+  url: `https://api.github.com/repos/telus/${process.env.GITHUB_REPOSITORY}/issues/${/[^/]*$/.exec(process.env.GITHUB_PULL_REQUEST)[0]
+    }/comments`,
+  headers: {
+    'User-Agent': 'TDS',
+    Authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
+  json: true,
+  body: {
+    body: `\`\`\`
+${changes.trim()}
+\`\`\``,
+  },
+})

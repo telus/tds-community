49307074696a3d94af45ac660702fa122efeb074
Layne Geck <layne.geck@telus.com>
2022-02-17 14:36:55 -0800

    fix: gha

--- scripts/gha/github-pr.js
+++ scripts/gha/github-pr.js
@@ -15,17 +15,16 @@ if (
   !process.env.GITHUB_PULL_REQUEST
 ) {
   console.error(
-    "'GITHUB_TOKEN', 'GITHUB_REPOSITORY'., and 'GITHUB_PULL_REQUEST' must be available in the environment."
+    "'GITHUB_TOKEN', 'GITHUB_REPOSITORY', and 'GITHUB_PULL_REQUEST' must be available in the environment."
   )
   process.exit(1)
 }
 
-const { output } = spawnSync('scripts/prePr.sh')
+const { output } = spawnSync('../prePr.sh')
 
 const changes = `Packages pending updates:\n${formattedVersions}\n\nIf this is not what you expected, ensure that your commit messages follow the TDS commit types guide on this page: https://tds.telus.com/contributing/developer-guide.html and try again.`
 request.post({
-  url: `https://api.github.com/repos/telus/${process.env.GITHUB_REPOSITORY}/issues/${/[^/]*$/.exec(process.env.GITHUB_PULL_REQUEST)[0]
-    }/comments`,
+  url: `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/issues/${process.env.GITHUB_PULL_REQUEST}/comments`,
   headers: {
     'User-Agent': 'TDS',
     Authorization: `token ${process.env.GITHUB_TOKEN}`,
